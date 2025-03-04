605ea4e7ab3e37c7bf4817b875755f8cf528788e
fareastern <36926187+fareastern@users.noreply.github.com>
2025-03-04 12:32:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberGuessed = 0; // количество угаданных раз
+
+        // играем
+        while (true) {
+            System.out.println("Введите год в формате yyyy: ");
+            int year = scanner.nextInt();
+            System.out.println("Введите количество дней в формате yyy: ");
+            int enteredDays = scanner.nextInt();
+            int correctDays = amount_days.checkYear(year);
+            if (enteredDays != correctDays){
+                System.out.println("Неправильно! В этом году "+correctDays+" дней!");
+                break;
+            }
+            numberGuessed++;
+        }
+
+        // выводим количество угаданных раз
+        System.out.println("Набрано очков: "+numberGuessed);
+    }
+}
